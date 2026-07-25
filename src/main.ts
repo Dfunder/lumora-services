98a80191d85b2eaec12b9410f128430814e6d9e9
codesailor4 <codesailor4@gmail.com>
2026-07-25 07:41:16 +0100

    implemented the api docs

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,46 @@
 import { NestFactory } from '@nestjs/core';
 import { BadRequestException, ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { GlobalHttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ValidationError } from 'class-validator';
 import { AppModule } from './app.module';
+import * as Sentry from '@sentry/node';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Initialize Sentry
+  if (process.env.SENTRY_DSN) {
+    Sentry.init({
+      dsn: process.env.SENTRY_DSN,
+      environment: process.env.NODE_ENV || 'development',
+    });
+  }
+
+  // Set up Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Lumora API')
+    .setDescription('Lumora services API documentation')
+    .setVersion('1.0')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+        name: 'JWT',
+        description: 'Enter JWT token',
+        in: 'header',
+      },
+      'JWT-auth',
+    )
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      security: [{ 'JWT-auth': [] }],
+    },
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
