5b7f2f932ee04da8f8f6b3e6c8a73179dd18fd3b
nafiuishaaq <nafiuig@gmail.com>
2026-08-20 16:10:28 +0100

    implemented the Donation recording with on-chain transaction verification

--- src/donation/donation.module.ts
+++ src/donation/donation.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { BullModule } from '@nestjs/bull';
+import { ConfigModule } from '@nestjs/config';
 import { DonationController } from './donation.controller';
 import { DonationService } from './donation.service';
 import { PrismaModule } from '../prisma/prisma.module';
@@ -7,6 +8,7 @@ import { PrismaModule } from '../prisma/prisma.module';
 @Module({
   imports: [
     PrismaModule,
+    ConfigModule,
     BullModule.registerQueue({ name: 'analytics' }),
   ],
   controllers: [DonationController],
